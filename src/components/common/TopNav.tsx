03c964640aae2b0c1ae8f54aaf5d15853beccec2
Philippe Fanaro <philippefanaro@gmail.com>
2024-04-16 08:40:56 -0300

    minor edit

--- src/components/common/TopNav.tsx
+++ src/components/common/TopNav.tsx
@@ -10,22 +10,30 @@ import { Button } from "@shad"
 export function TopNav() {
   return (
     <nav className="absolute top-0 w-screen p-3 flex justify-between">
-      <div></div>
-
-      <div className="flex gap-2 items-center">
-        <Button>Regen Data</Button>
-
-        <div className="flex items-center">
-          <SignedOut>
-            <Button>
-              <SignInButton />
-            </Button>
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
-        </div>
-      </div>
+      <LeftTopnav />
+      <RightTopnav />
     </nav>
+  )
+}
+
+function LeftTopnav() {
+  return <div></div>
+}
+
+function RightTopnav() {
+  return (
+    <div className="flex gap-2 items-center">
+      <Button>Regen Data</Button>
+      <div className="flex items-center">
+        <SignedOut>
+          <Button>
+            <SignInButton />
+          </Button>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </div>
+    </div>
   )
 }
