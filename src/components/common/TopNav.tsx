0887ae19c5230157f84aa250dd3415223349e672
Philippe Fanaro <philippefanaro@gmail.com>
2024-04-19 10:02:56 -0300

    minor fix to the top bar

--- src/components/common/TopNav.tsx
+++ src/components/common/TopNav.tsx
@@ -11,7 +11,7 @@ import { ThemeButton } from "./ThemeButton"
 
 export function TopNav() {
   return (
-    <nav className="z-50 absolute top-1 w-screen p-4 flex justify-between">
+    <nav className="absolute top-1 w-screen p-4 flex justify-between">
       <LeftTopnav />
       <RightTopnav />
     </nav>
@@ -24,7 +24,7 @@ function LeftTopnav() {
 
 function RightTopnav() {
   return (
-    <div className="flex gap-2 items-center">
+    <div className="flex gap-2 items-center z-50">
       <ThemeButton />
       <SignedOut>
         <Button>
